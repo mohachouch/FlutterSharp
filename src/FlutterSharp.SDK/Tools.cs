9c89159307c77e1193366a6f8481da8ee3bb9a47
Mohamed CHOUCHANE <mohamed.chouchane1990@gmail.com>
2019-09-29 23:05:55 +0200

    Add gitignore

---
+++ src/FlutterSharp.SDK/Tools.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlutterSharp.SDK
+{
+    public class Map<T, T1> : Dictionary<T, T1>
+    {
+
+    }
+
+    public class Iterable<T>
+    {
+
+    }
+
+    public class Uint8Buffer
+    {
+
+    }
+}
